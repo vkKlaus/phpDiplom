70cef1e031b91e994de2adbab7c0a128fcbf0591
vkKlaus <vkKlaus@yandex.ru>
2020-06-11 14:34:02 +0300

    Update products.php

--- views/admin/products.php
+++ views/admin/products.php
@@ -2,9 +2,18 @@
 require $_SERVER['DOCUMENT_ROOT'] . '/views/layouts/index.php';
 resetFilterSession();
 
+$products = getTable($pdo, 'product');
+
 require  $_SERVER['DOCUMENT_ROOT'] . '/views/layouts/header.php';
 ?>
-
-продукты
-
+<?php foreach ($products as $product) { ?>
+    <div class="row">
+        <div class="col-2">
+            <img src="/images/products/<?= $product['id'] ?>.jpg" alt=""  class="card_img"  >
+        </div>
+        <div class="col-10">
+            <
+        </div>
+    </div>
+<?php } ?>
 <?php require $_SERVER['DOCUMENT_ROOT'] . '/views/layouts/footer.php';
